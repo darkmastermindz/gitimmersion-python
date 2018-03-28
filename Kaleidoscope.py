633fcfdae61987520d1917e85fdb852f94661d15
Hansel Wei <darkmastermindz@gmail.com>
2018-03-28 18:42:29 -0400

    Added Function 1

--- Kaleidoscope.py
+++ Kaleidoscope.py
@@ -1,2 +1,24 @@
 import random
 import turtle
+
+t = turtle.Pen()
+t.speed(0)
+t.hideturtle()
+turtle.bgcolor("black")
+colors = ["red", "blue", "green", "orange", "purple", "white", "gray"]
+
+# Function 1
+
+draw_kaleido(x,y):
+    t.pencolor(random.choice(colors))
+    size = random.randint(10,40)
+    draw_spiral(x,y, size)
+    draw_spiral(-x,y, size)
+    draw_spiral(-x,-y, size)
+    draw_spiral(x,-y, size)
+
+# Function 2
+
+
+turtle.onscreenclick(draw_kaleido)
+
